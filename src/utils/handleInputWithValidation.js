0ffc5df9872a5a09d3bbe14239f6396db304f124
CJY <cjy3458@naver.com>
2024-12-13 18:57:31 +0900

    feat: 입력과 유효성 검증 함수 구현현

---
+++ src/utils/handleInputWithValidation.js
@@ -0,0 +1,14 @@
+import OutputView from '../views/OutputView.js';
+
+async function handleInputWithValidator(inputFn, validatorFn, ...args) {
+  try {
+    const input = await inputFn();
+    validatorFn(input, ...args);
+    return await inputFn;
+  } catch (error) {
+    OutputView.printError(error);
+    return handleInputWithValidator(inputFn, validatorFn, ...args);
+  }
+}
+
+export default handleInputWithValidator;
