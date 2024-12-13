1b6398f52909eb2fd5e68d90e33f33dcbd5c83a0
CJY <cjy3458@naver.com>
2024-12-13 19:08:26 +0900

    feat: 요일 객체 상수값 구현

---
+++ src/constants/config.js
@@ -0,0 +1,9 @@
+export const WEEK = Object.freeze([
+  { dayOftheWeek: '월', prder: 1 },
+  { dayOftheWeek: '화', prder: 2 },
+  { dayOftheWeek: '수', prder: 3 },
+  { dayOftheWeek: '목', prder: 4 },
+  { dayOftheWeek: '금', prder: 5 },
+  { dayOftheWeek: '토', prder: 6 },
+  { dayOftheWeek: '일', prder: 7 },
+]);
